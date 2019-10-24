654cbaf263f8fbd21e3fe7ef5b52f3ef2f64e749
Simon <simonolsen388@gmail.com>
2019-10-24 21:19:04 +0000

    removed commented out part

--- breedFetcher.js
+++ breedFetcher.js
@@ -11,12 +11,3 @@ request(url, (error, response, body) => {
     console.log("please enter a name");
   }
 });
-
-// request(
-//   "https://api.thecatapi.com/v1/breeds/search?q=sib",
-//   (error, response, body) => {
-//     const data = JSON.parse(body);
-//     console.log(data[0].name);
-//     console.log(typeof data);
-//   }
-// );
