a96db84ab785751b416104bd4ae523eb8bc95de9
Simon <simonolsen388@gmail.com>
2019-10-24 23:43:32 +0000

    added tests and refactored our code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,17 @@
 const request = require("request");
-const args = process.argv.slice(2);
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${args}`;
+// breedName takes care of process.argv[2] on index.js
+// have to follow the callback syntax from index.js (look at line 10 for example. and line 5 for what the callback looks like.)
 
-request(url, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (data.length > 0) {
-    console.log("statusCode:", response && response.statusCode);
-    console.log(data[0].description);
-  } else if (data.length <= 0) {
-    console.log("please enter a name");
-  }
-});
+const fetchBreedDescription =
+  (breedName, callback) => {
+    request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+      const data = JSON.parse(body);
+      if (data.length > 0) {
+        callback(error, data[0].description);
+      } else if (data.length === 0) {
+        callback("please enter a name");
+      }
+    });
+  };
+
+module.exports = { fetchBreedDescription };
