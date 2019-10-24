6987e4e1d75a69345d4c5579de18ec1ac2307803
Simon <simonolsen388@gmail.com>
2019-10-24 21:17:49 +0000

    implemented api

---
+++ breedFetcher.js
@@ -0,0 +1,22 @@
+const request = require("request");
+const args = process.argv.slice(2);
+let url = `https://api.thecatapi.com/v1/breeds/search?q=${args}`;
+
+request(url, (error, response, body) => {
+  const data = JSON.parse(body);
+  if (data.length > 0) {
+    console.log("statusCode:", response && response.statusCode);
+    console.log(data[0].description);
+  } else if (data.length <= 0) {
+    console.log("please enter a name");
+  }
+});
+
+// request(
+//   "https://api.thecatapi.com/v1/breeds/search?q=sib",
+//   (error, response, body) => {
+//     const data = JSON.parse(body);
+//     console.log(data[0].name);
+//     console.log(typeof data);
+//   }
+// );
